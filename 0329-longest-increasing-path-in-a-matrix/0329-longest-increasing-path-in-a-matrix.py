e7dc3af5cbcf65d8ab13271914de990955ea048c
Le Quang Hoai <99666261+Hoaihx123@users.noreply.github.com>
2024-01-28 19:37:52 +0300

    Time: 244 ms (94.34%), Space: 12.4 MB (97.43%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -4,45 +4,21 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: int
         """
-        def findMax(matrix,m,n,max_before):
-            max_value=float('-inf')
-            index=[]
-            for i in range(m):
-                for j in range(n):
-                    if matrix[i][j]>max_value and matrix[i][j]<max_before:
-                        max_value=matrix[i][j]
-                        index=[(i,j)]
-                    elif matrix[i][j]==max_value:
-                        index.append((i,j))
-            return {'index': index, 'max_value': max_value}
         m = len(matrix)
         n = len(matrix[0])
-        count = [[None for _ in range(n)] for _ in range(m)]
-        max_before = float('inf')
-        fm = findMax(matrix, m, n, max_before)
+        def recusive(i,j):
+            if count[i][j]==0:
+                count[i][j]=1+max(recusive(i-1,j) if i > 0 and matrix[i][j] < matrix[i-1][j] else 0, 
+                                  recusive(i+1,j) if i <m-1 and matrix[i][j] < matrix[i+1][j] else 0,
+                                  recusive(i,j-1) if j > 0 and matrix[i][j] < matrix[i][j-1] else 0,
+                                  recusive(i,j+1) if j <n-1 and matrix[i][j] < matrix[i][j+1] else 0,0)
+            return count[i][j]
+        
+        count = [[0 for _ in range(n)] for _ in range(m)]
         result=0
-        while fm['index']:
-            for u in fm['index']:
-                point = None
-                longest = 1
-                if u[0] > 0 and matrix[u[0]][u[1]] < matrix[u[0] - 1][u[1]]:
-                    longest = 1 + count[u[0] - 1][u[1]][0]
-                    point = (u[0] - 1, u[1])
-                if u[0] < m - 1 and matrix[u[0]][u[1]] < matrix[u[0] + 1][u[1]]:
-                    if count[u[0] + 1][u[1]][0] + 1 > longest:
-                        longest = 1 + count[u[0] + 1][u[1]][0]
-                        point = (u[0] + 1, u[1])
-                if u[1] > 0 and matrix[u[0]][u[1]] < matrix[u[0]][u[1] - 1]:
-                    if count[u[0]][u[1] - 1][0] + 1 > longest:
-                        longest = 1 + count[u[0]][u[1] - 1][0]
-                        point = (u[0], u[1] - 1)
-                if u[1] < n - 1 and matrix[u[0]][u[1]] < matrix[u[0]][u[1] + 1]:
-                    if count[u[0]][u[1] + 1][0] + 1 > longest:
-                        longest = 1 + count[u[0]][u[1] + 1][0]
-                        point = (u[0], u[1] + 1)
-                result=max(longest,result)
-                count[u[0]][u[1]] = (longest, point)
-            fm = findMax(matrix, m, n, fm['max_value'])
+        for i in range(m):
+            for j in range(n):
+                result=max(result,recusive(i,j))
         return result
         
                 
