d2d38a5fa4ade719d196d271ef88dc12dbbdd9d1
Le Quang Hoai <99666261+Hoaihx123@users.noreply.github.com>
2024-01-31 01:39:26 +0300

    Time: 18 ms (93.87%), Space: 11.7 MB (93.79%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        n=len(s)
+        m=len(p)
+        dp=[[False for _ in range(m+1)] for _ in range(n+1)]
+        dp[0][0]=True
+        for i in range(n+1):
+            for j in range(1,m+1):
+                if p[j-1]=='*':
+                    dp[i][j]=dp[i][j-2] or (dp[i-1][j] and (s[i-1]==p[j-2] or p[j-2]=='.'))
+                elif p[j-1]=='.' or p[j-1]==s[i-1]:
+                    dp[i][j]=dp[i-1][j-1]
+        return dp[n][m]
