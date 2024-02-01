410d69dac95c5fc14f0d8ae40de77a3c3038290b
Le Quang Hoai <99666261+Hoaihx123@users.noreply.github.com>
2024-02-01 16:47:44 +0300

    Time: 24 ms (66.56%), Space: 12.3 MB (95.53%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -4,43 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        stack=[]
-        for c in s:
-            if c=='(':
-                stack.append(c)
+        stack=[-1]
+        result=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
             else:
+                stack.pop()
                 if stack:
-                    if stack[-1]=='(':
-                        stack.pop()
-                        if stack:
-                            if stack[-1]=='(' or stack[-1]==')':
-                                stack.append(2)
-                            else:
-                                stack.append(stack.pop()+2)
-                        else:
-                            stack.append(2)
-                    elif stack[-1]==')':
-                        pass
-                    else:
-                        if len(stack)>=2 and stack[-2]=='(':
-                            if len(stack)>=3 and stack[-3] not in ['(', ')']:
-                                temp=stack.pop()
-                                stack.pop()
-                                stack.append(stack.pop()+2+temp)
-                            else:
-                                temp=stack.pop()
-                                stack.pop()
-                                stack.append(temp+2)
-                        else:
-                            stack.append(c)
+                    result = max(result, i-stack[-1])
                 else:
-                    stack.append(c)
-        max=0
-        for x in stack:
-            if x not in [')', '('] and x>max:
-                max=x
-
-        return max
+                    stack.append(i)
+        return result
 
                             
                         
