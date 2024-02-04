6a8f3d624adefc6189518b6fbe425ff4f5a0ea38
Le Quang Hoai <99666261+Hoaihx123@users.noreply.github.com>
2024-02-04 21:23:56 +0300

    Time: 8019 ms (5.47%), Space: 12.2 MB (96.69%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        m = len(words[0])
+        n = len(words)
+        l = len(s)
+        dict1 = {}
+        result = []
+        for w in words:
+            dict1[w] = dict1.get(w, 0) + 1
+        for i in range(m):
+            start = i
+            while start + m * n <= l:
+                dict2 = {}
+                j=start
+                while j <start+m*n:
+                    if dict1.get(s[j:j + m], 0) > dict2.get(s[j:j + m], 0):
+                        dict2[s[j:j + m]] = dict2.get(s[j:j + m], 0) + 1
+                        j+=m
+                    else:
+                        break
+                print(j)
+                if j < start + m * n:
+                    if dict1.get(s[j:j + m]):
+                        start += m
+                    else:
+                        start = j + m
+                else:
+                    result.append(start)
+                    start += m
+        return result
+
